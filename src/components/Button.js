4f344477c3014c5a0d39a9e4a1eee01de6124db9
Anja Seeba <ahoi@achtsam.media>
2021-02-04 12:16:55 +0100

    Create Button component

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import './Button.css'
+
+export default function Button() {
+  const el = document.createElement('button')
+  el.className = 'Button'
+  el.innerText = Text
+  return el
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log('hello world')
+
+import Button from './components/Button'
+
+const button = Button('Click me')
+document.body.append(button)
